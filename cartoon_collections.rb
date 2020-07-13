b254426daf8627a0a2734524c57ff71ffc51a9de
Elliott Stein <estein1988@gmail.com>
2020-07-13 19:53:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.inject("!") do |word|
+  planeteer_calls.each do |word|
     word.capitalize + "!"
   end
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
