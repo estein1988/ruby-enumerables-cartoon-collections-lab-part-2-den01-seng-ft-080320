eddc3a773b0877e13c10a469e7ba59d4499f742b
Elliott Stein <estein1988@gmail.com>
2020-07-13 20:00:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.each do |word|
+  planeteer_calls.map do |word|
     word.length > 4
   end
 end
