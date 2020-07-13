f0fd05a521397b9dd870b89dcc825607f97760a4
Elliott Stein <estein1988@gmail.com>
2020-07-13 20:10:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |word|
-    word.include?
+    word.include? word = word
   end
 end
